c09be6a526f44f4342c403ee3cc709e34440c50c
SanHanona <shanon19@student.aau.dk>
2022-03-08 09:31:17 +0100

    fix: first math bug (sqrt.atan)

---
+++ exomy_ws/src/build/exomy_sim_launch/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/exomy/exomy_ws/exomy_ws_ros/exomy_ws/src/exomy_sim_launch
+# Build directory: /home/exomy/exomy_ws/exomy_ws_ros/exomy_ws/src/build/exomy_sim_launch
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
