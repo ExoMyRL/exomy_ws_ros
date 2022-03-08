fix: steering directions (missing scaling factor forward/backward)

exomy_ws/src/exomy/exomy/rover_modified.py
@@ -21,17 +21,17 @@ class Rover():
         self.wheel_x = 12.0
         self.wheel_y = 20.0
 
-    def Set_SteeringAngles(self, lin_vel_x, lin_vel_y, ang_vel):
+    def Set_SteeringAngles(self, lin_vel_x, ang_vel):
         '''
         Converts the steering command [angle of joystick] to angles for the different motors
         '''
         steering_angles = [0]*6
         motor_speeds = [0]*6
         # Determine radius of turning point (from the center of the rover) (Centimeters):
-        lin_vel = math.sqrt((lin_vel_x*lin_vel_x)+(lin_vel_y*lin_vel_y)) # Jeg er ikke sikker paa vi kan goere det saadan her
+        # lin_vel = math.sqrt((lin_vel_x*lin_vel_x)+(lin_vel_y*lin_vel_y)) # Jeg er ikke sikker paa vi kan goere det saadan her
         
         if ang_vel != 0:
-            radius = (lin_vel/abs(ang_vel))*10
+            radius = (abs(lin_vel_x)/abs(ang_vel))*10
 
         # Distance from center of the rover to the top (centimeters):
         y_top = 20 # check if it's correct
@@ -57,7 +57,7 @@ class Rover():
             steering_angles[self.RL] = -math.atan2(y_top,x_side)
             steering_angles[self.RR] = math.atan2(y_top,x_side)
         # If the rotation is positive, we turn anticlockwise
-        elif ang_vel >= 0:
+        elif (ang_vel > 0 and np.sign(lin_vel_x) > 0) or (ang_vel < 0 and np.sign(lin_vel_x) < 0):
             steering_angles[self.FL] = -math.atan2(y_top,(radius-x_side))
             steering_angles[self.FR] = -math.atan2(y_top,(radius+x_side))
             steering_angles[self.CL] = 0
@@ -65,7 +65,7 @@ class Rover():
             steering_angles[self.RL] = math.atan2(y_top,(radius-x_side))
             steering_angles[self.RR] = math.atan2(y_top,(radius+x_side))
         # If the rotation is negativ, we turn clockwise
-        elif ang_vel < 0:
+        elif (ang_vel < 0 and np.sign(lin_vel_x) > 0) or (ang_vel > 0 and np.sign(lin_vel_x) < 0):
             steering_angles[self.FL] = math.atan2(y_top,(radius+x_side))
             steering_angles[self.FR] = math.atan2(y_top,(radius-x_side))
             steering_angles[self.CL] = 0
@@ -76,44 +76,45 @@ class Rover():
         """
         Motor speeds calculation
         """
-        # Speed turning in place:
+        # Speed moving forward/backward
         if ang_vel == 0: 
-            motor_speeds[self.FL] = lin_vel
-            motor_speeds[self.FR] = lin_vel
-            motor_speeds[self.CL] = lin_vel
-            motor_speeds[self.CR] = lin_vel
-            motor_speeds[self.RL] = lin_vel
-            motor_speeds[self.RR] = lin_vel
-        if radius <= 15 and ang_vel >= 0: # anticlockwise
-            motor_speeds[self.FL] = -math.sqrt((y_top*y_top)+(x_side*x_side))*ang_vel
-            motor_speeds[self.FR] = math.sqrt((y_top*y_top)+(x_side*x_side))*ang_vel
-            motor_speeds[self.CL] = -x_side*ang_vel
-            motor_speeds[self.CR] = x_side*ang_vel
-            motor_speeds[self.RL] = -math.sqrt((y_top*y_top)+(x_side*x_side))*ang_vel
-            motor_speeds[self.RR] = math.sqrt((y_top*y_top)+(x_side*x_side))*ang_vel
+            motor_speeds[self.FL] = lin_vel_x*30 # fix skalerings faktor
+            motor_speeds[self.FR] = lin_vel_x*30 # fix skalerings faktor
+            motor_speeds[self.CL] = lin_vel_x*30 # fix skalerings faktor
+            motor_speeds[self.CR] = lin_vel_x*30 # fix skalerings faktor
+            motor_speeds[self.RL] = lin_vel_x*30 # fix skalerings faktor
+            motor_speeds[self.RR] = lin_vel_x*30 # fix skalerings faktor
+        # Speed turning in place
+        elif radius <= 15 and ang_vel > 0: # anticlockwise
+            motor_speeds[self.FL] = -math.sqrt((y_top*y_top)+(x_side*x_side))*abs(ang_vel)
+            motor_speeds[self.FR] = math.sqrt((y_top*y_top)+(x_side*x_side))*abs(ang_vel)
+            motor_speeds[self.CL] = -x_side*abs(ang_vel)
+            motor_speeds[self.CR] = x_side*abs(ang_vel)
+            motor_speeds[self.RL] = -math.sqrt((y_top*y_top)+(x_side*x_side))*abs(ang_vel)
+            motor_speeds[self.RR] = math.sqrt((y_top*y_top)+(x_side*x_side))*abs(ang_vel)
         elif radius <= 15 and ang_vel < 0: # Clockwise
-            motor_speeds[self.FL] = math.sqrt((y_top*y_top)+(x_side*x_side))*ang_vel
-            motor_speeds[self.FR] = -math.sqrt((y_top*y_top)+(x_side*x_side))*ang_vel
-            motor_speeds[self.CL] = x_side*ang_vel
-            motor_speeds[self.CR] = -x_side*ang_vel
-            motor_speeds[self.RL] = math.sqrt((y_top*y_top)+(x_side*x_side))*ang_vel
-            motor_speeds[self.RR] = -math.sqrt((y_top*y_top)+(x_side*x_side))*ang_vel
+            motor_speeds[self.FL] = math.sqrt((y_top*y_top)+(x_side*x_side))*abs(ang_vel)
+            motor_speeds[self.FR] = -math.sqrt((y_top*y_top)+(x_side*x_side))*abs(ang_vel)
+            motor_speeds[self.CL] = x_side*abs(ang_vel)
+            motor_speeds[self.CR] = -x_side*abs(ang_vel)
+            motor_speeds[self.RL] = math.sqrt((y_top*y_top)+(x_side*x_side))*abs(ang_vel)
+            motor_speeds[self.RR] = -math.sqrt((y_top*y_top)+(x_side*x_side))*abs(ang_vel)
         # Speed turning anticlockwise:
         elif ang_vel >= 0:
-            motor_speeds[self.FL] = math.sqrt((y_top*y_top)+((radius-x_side)*(radius-x_side)))*ang_vel
-            motor_speeds[self.FR] = math.sqrt((y_top*y_top)+((radius+x_side)*(radius+x_side)))*ang_vel
-            motor_speeds[self.CL] = (radius-x_side)*ang_vel
-            motor_speeds[self.CR] = (radius+x_side)*ang_vel
-            motor_speeds[self.RL] = math.sqrt((y_top*y_top)+((x_side-x_side)*(x_side-x_side)))*ang_vel
-            motor_speeds[self.RR] = math.sqrt((y_top*y_top)+((x_side+x_side)*(x_side+x_side)))*ang_vel
+            motor_speeds[self.FL] = (math.sqrt((y_top*y_top)+((radius-x_side)*(radius-x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
+            motor_speeds[self.FR] = (math.sqrt((y_top*y_top)+((radius+x_side)*(radius+x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
+            motor_speeds[self.CL] = ((radius-x_side)*abs(ang_vel))*np.sign(lin_vel_x)
+            motor_speeds[self.CR] = ((radius+x_side)*abs(ang_vel))*np.sign(lin_vel_x)
+            motor_speeds[self.RL] = (math.sqrt((y_top*y_top)+((x_side-x_side)*(x_side-x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
+            motor_speeds[self.RR] = (math.sqrt((y_top*y_top)+((x_side+x_side)*(x_side+x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
         # Speed turning clockwise
         elif ang_vel < 0:
-            motor_speeds[self.FL] = math.sqrt((y_top*y_top)+((radius+x_side)*(radius+x_side)))*ang_vel
-            motor_speeds[self.FR] = math.sqrt((y_top*y_top)+((radius-x_side)*(radius-x_side)))*ang_vel
-            motor_speeds[self.CL] = (radius+x_side)*ang_vel
-            motor_speeds[self.CR] = (radius-x_side)*ang_vel
-            motor_speeds[self.RL] = math.sqrt((y_top*y_top)+((radius+x_side)*(radius+x_side)))*ang_vel
-            motor_speeds[self.RR] = math.sqrt((y_top*y_top)+((radius-x_side)*(radius-x_side)))*ang_vel
+            motor_speeds[self.FL] = (math.sqrt((y_top*y_top)+((radius+x_side)*(radius+x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
+            motor_speeds[self.FR] = (math.sqrt((y_top*y_top)+((radius-x_side)*(radius-x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
+            motor_speeds[self.CL] = ((radius+x_side)*abs(ang_vel))*np.sign(lin_vel_x)
+            motor_speeds[self.CR] = ((radius-x_side)*abs(ang_vel))*np.sign(lin_vel_x)
+            motor_speeds[self.RL] = (math.sqrt((y_top*y_top)+((radius+x_side)*(radius+x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
+            motor_speeds[self.RR] = (math.sqrt((y_top*y_top)+((radius-x_side)*(radius-x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
 
     
         motor_speeds[self.FL] = int(motor_speeds[self.FL])
@@ -123,15 +124,11 @@ class Rover():
         motor_speeds[self.RL] = int(motor_speeds[self.RL])
         motor_speeds[self.RR] = int(motor_speeds[self.RR])
 
-        print(motor_speeds[self.FL])
-
         steering_angles[self.FL] = int(np.rad2deg(steering_angles[self.FL]))
         steering_angles[self.FR] = int(np.rad2deg(steering_angles[self.FR]))
         steering_angles[self.CL] = int(np.rad2deg(steering_angles[self.CL]))
         steering_angles[self.CR] = int(np.rad2deg(steering_angles[self.CR]))
         steering_angles[self.RL] = int(np.rad2deg(steering_angles[self.RL]))
         steering_angles[self.RR] = int(np.rad2deg(steering_angles[self.RR]))
-
-        print(steering_angles)
     
         return steering_angles, motor_speeds