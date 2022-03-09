55afd7914fa886e1821e0317d10f8ef2d9028dd7
Bonnerup123 <sorenbonnerup@outlook.com>
2022-03-09 15:22:41 +0100

    fix: comments in rover_modified

--- exomy_ws/src/exomy/exomy/rover_modified.py
+++ exomy_ws/src/exomy/exomy/rover_modified.py
@@ -33,12 +33,13 @@ class Rover():
 
         # Distance from center og the rover to the top (centimeters):
         y_top = 19.5 # check if it's correct
-        # Distance from center to the side:
+        # Distance from center of the rover to the side (centimeters):
         x_side = 15 # check if it's correct
 
         """
         Steering angles calculation 
         """
+        # If the angular velociy is 0, the angles for the wheel are set to 0
         if ang_vel == 0: 
             steering_angles[self.FL] = 0
             steering_angles[self.FR] = 0
@@ -54,7 +55,7 @@ class Rover():
             steering_angles[self.CR] = 0
             steering_angles[self.RL] = -math.atan2(y_top,x_side)
             steering_angles[self.RR] = math.atan2(y_top,x_side)
-        # If the rotation is positive, we turn anticlockwise
+        # Steering angles if turning anticlockwise moving forward or clockwise moving backwards
         elif (ang_vel > 0 and np.sign(lin_vel_x) > 0) or (ang_vel < 0 and np.sign(lin_vel_x) < 0):
             steering_angles[self.FL] = -math.atan2(y_top,(radius-x_side))
             steering_angles[self.FR] = -math.atan2(y_top,(radius+x_side))
@@ -62,7 +63,7 @@ class Rover():
             steering_angles[self.CR] = 0
             steering_angles[self.RL] = math.atan2(y_top,(radius-x_side))
             steering_angles[self.RR] = math.atan2(y_top,(radius+x_side))
-        # If the rotation is negativ, we turn clockwise
+        # Steering angles if turning clockwise moving forward or anticlockwise moving backwards
         elif (ang_vel < 0 and np.sign(lin_vel_x) > 0) or (ang_vel > 0 and np.sign(lin_vel_x) < 0):
             steering_angles[self.FL] = math.atan2(y_top,(radius+x_side))
             steering_angles[self.FR] = math.atan2(y_top,(radius-x_side))
@@ -104,29 +105,30 @@ class Rover():
             motor_speeds[self.CR] = -abs(ang_vel)*relation
             motor_speeds[self.RL] = abs(ang_vel)
             motor_speeds[self.RR] = -abs(ang_vel)
+        # Speed turning anticlockwise moving forward/backward, velocity of frontRight wheel = linear velocity 
         elif ang_vel > 0:
             frontLeft = (math.sqrt((y_top*y_top)+((radius-x_side)*(radius-x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
             frontRight = (math.sqrt((y_top*y_top)+((radius+x_side)*(radius+x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
-            frontRelation = frontLeft/frontRight
+            frontRelation = frontLeft/frontRight # relation of speed between the front wheels (frontLeft is slower)
             centerLeft = ((radius-x_side)*abs(ang_vel))*np.sign(lin_vel_x)
             centerRight = ((radius+x_side)*abs(ang_vel))*np.sign(lin_vel_x)
-            centerRelation = centerLeft/centerRight
-            frontCenterRelation = centerRight/frontRight
+            centerRelation = centerLeft/centerRight # relation of speed between the center wheels (centerLeft is slower)
+            frontCenterRelation = centerRight/frontRight # relation between center and front wheels (center is slower)
             motor_speeds[self.FL] = lin_vel_x*frontRelation
             motor_speeds[self.FR] = lin_vel_x
             motor_speeds[self.CL] = lin_vel_x*frontCenterRelation*centerRelation
             motor_speeds[self.CR] = lin_vel_x*frontCenterRelation
             motor_speeds[self.RL] = lin_vel_x*frontRelation
             motor_speeds[self.RR] = lin_vel_x
-        # Speed turning clockwise
+        # Speed turning clockwise moving forward/backward, velocity of frontLeft wheel = linear velocity
         elif ang_vel < 0:
             frontLeft = (math.sqrt((y_top*y_top)+((radius+x_side)*(radius+x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
             frontRight = (math.sqrt((y_top*y_top)+((radius-x_side)*(radius-x_side)))*abs(ang_vel))*np.sign(lin_vel_x)
-            frontRelation = frontRight/frontLeft
+            frontRelation = frontRight/frontLeft # relation of speed between the front wheels (frontRight is slower)
             centerLeft = ((radius+x_side)*abs(ang_vel))*np.sign(lin_vel_x)
             centerRight = ((radius-x_side)*abs(ang_vel))*np.sign(lin_vel_x)
-            centerRelation = centerRight/centerLeft
-            frontCenterRelation = centerLeft/frontLeft
+            centerRelation = centerRight/centerLeft # relation of speed between the center wheels (centerRight is slower)
+            frontCenterRelation = centerLeft/frontLeft # relation between center and front wheels (center is slower)
             motor_speeds[self.FL] = lin_vel_x
             motor_speeds[self.FR] = lin_vel_x*frontRelation
             motor_speeds[self.CL] = lin_vel_x*frontCenterRelation
@@ -134,7 +136,7 @@ class Rover():
             motor_speeds[self.RL] = lin_vel_x
             motor_speeds[self.RR] = lin_vel_x*frontRelation
 
-    
+        # Motor speeds are converted to int's
         motor_speeds[self.FL] = int(motor_speeds[self.FL])
         motor_speeds[self.FR] = int(motor_speeds[self.FR])
         motor_speeds[self.CL] = int(motor_speeds[self.CL])
@@ -142,6 +144,7 @@ class Rover():
         motor_speeds[self.RL] = int(motor_speeds[self.RL])
         motor_speeds[self.RR] = int(motor_speeds[self.RR])
 
+        # Steering angles are first converted to degrees and then to int's
         steering_angles[self.FL] = int(np.rad2deg(steering_angles[self.FL]))
         steering_angles[self.FR] = int(np.rad2deg(steering_angles[self.FR]))
         steering_angles[self.CL] = int(np.rad2deg(steering_angles[self.CL]))
@@ -149,5 +152,4 @@ class Rover():
         steering_angles[self.RL] = int(np.rad2deg(steering_angles[self.RL]))
         steering_angles[self.RR] = int(np.rad2deg(steering_angles[self.RR]))
     
-
         return steering_angles, motor_speeds
